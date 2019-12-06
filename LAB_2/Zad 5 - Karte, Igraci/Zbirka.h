6996f6daf1589f61577218cb52aa7785ec64bc93
Nikola Cvetanovic <44877969+NikolaCv@users.noreply.github.com>
2019-12-06 16:06:11 +0100

    Update Zbirka.h

--- LAB_2/Zad 5 - Karte, Igraci/Zbirka.h
+++ LAB_2/Zad 5 - Karte, Igraci/Zbirka.h
@@ -26,8 +26,9 @@ public:
 	Zbirka& operator+=(Karta * card);
 	Karta* operator[](int index) const;
 	Karta* operator[](unsigned id) const;
-	void operator()(int index);
-	void operator()(unsigned id);
+	void operator()(int index);	//must be used in pair with operator+= to ensure there's no memory leak!!! (it's just deleting the node itself, and not card in that node)
+	void operator()(unsigned id);	//because cards can't be copied, and when we are moving cards through player's collections
+					//we need the exact same card
 	void operator~();
 	friend ostream& operator<<(ostream & out, const Zbirka& collection);
 
@@ -35,8 +36,8 @@ private:
 	Node* prvi, *posl;
 	int len = 0;
 	void kopiraj(const Zbirka& collection);	//kada kopiramo pravimo novu kartu sa novim ID,
-											//i istim atributima, jer karta ne moze obicno da se kopira
-	void premesti(Zbirka& collection);		//ovde samo premestamo pokazivace
+						//i istim atributima, jer karta ne moze obicno da se kopira
+	void premesti(Zbirka& collection);	//ovde samo premestamo pokazivace
 	void brisi();
 };
 
