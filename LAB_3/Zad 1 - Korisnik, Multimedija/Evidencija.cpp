832faf045b7b39cef6acc64d1164ce68cf283b37
Nikola Cvetanovic <44877969+NikolaCv@users.noreply.github.com>
2019-12-23 20:42:04 +0100

    added evidencija functions

--- LAB_3/Zad 1 - Korisnik, Multimedija/Evidencija.cpp
+++ LAB_3/Zad 1 - Korisnik, Multimedija/Evidencija.cpp
@@ -2,12 +2,48 @@
 
 Evidencija & Evidencija::operator+=(Korisnik * user)
 {
+	for (int i = 0; i < list.getLength(); ++i)
+		if (list[i] == user) throw myExceptions::UserAlreadyExistsException();
+
 	(this->list) += user;
 	return *this;
 }
 
 Korisnik * Evidencija::operator[](const string name)
 {
+	for (int i = 0; i < list.getLength(); ++i)
+		if (list[i]->getName() == name)
+			return list[i];
 
-	return nullptr;
+	throw myExceptions::UserDoesntExistException();
+}
+
+Korisnik Evidencija::operator[](const string name) const
+{
+	for (int i = 0; i < list.getLength(); ++i)
+		if (list[i]->getName() == name)
+			return *list[i];
+
+	throw myExceptions::UserDoesntExistException();
+}
+
+void Evidencija::operator()(const string name)
+{
+	for (int i = 0; i < list.getLength(); ++i)
+		if (list[i]->getName() == name)
+			list(i);
+
+	throw myExceptions::UserDoesntExistException();
+}
+
+void Evidencija::changePassword(const string name, const string old_password, const string new_password)
+{
+	for (int i = 0; i < list.getLength(); ++i)
+		if (list[i]->getName() == name)
+		{
+			list[i]->changePassword(old_password, new_password);
+			return;
+		}
+
+	throw myExceptions::UserDoesntExistException();
 }
