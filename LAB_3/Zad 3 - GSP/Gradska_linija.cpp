d5d6c0a4403eb37f2a36b3365c33ed3378f535ed
Nikola Cvetanovic <44877969+NikolaCv@users.noreply.github.com>
2019-12-25 14:43:31 +0100

    Update Gradska_linija.cpp

--- LAB_3/Zad 3 - GSP/Gradska_linija.cpp
+++ LAB_3/Zad 3 - GSP/Gradska_linija.cpp
@@ -1,12 +1,17 @@
 #include "Gradska_linija.h"
 
-
-
-Gradska_linija::Gradska_linija()
+Gradska_linija::Gradska_linija(const string & text_tag, Lista<Stajaliste*> list):text_tag(text_tag),stations(list)
 {
 }
 
-
-Gradska_linija::~Gradska_linija()
+int operator&(const Gradska_linija & line1, const Gradska_linija & line2)
 {
+	int s = 0;
+
+	return s;
+}
+
+ostream & operator<<(ostream & out, const Gradska_linija & line)
+{
+	return out << line.text_tag << endl << line.stations;
 }
