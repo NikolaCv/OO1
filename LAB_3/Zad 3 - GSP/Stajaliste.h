1e2891da52c707abf4b1f40e3e4e0100f14c6a46
Nikola Cvetanovic <44877969+NikolaCv@users.noreply.github.com>
2019-12-25 14:43:07 +0100

    Update Stajaliste.h

--- LAB_3/Zad 3 - GSP/Stajaliste.h
+++ LAB_3/Zad 3 - GSP/Stajaliste.h
@@ -21,17 +21,15 @@ public:
 	string getName() const;
 	friend bool operator==(const Stajaliste& station1, const Stajaliste& station2);
 	friend bool operator!=(const Stajaliste& station1, const Stajaliste& station2);
+	void addLine(Gradska_linija* line);
 
 	friend ostream& operator<<(ostream& out, const Stajaliste& station);
 
 private:
 	int tag, zone;
 	string name;
-	Lista<Gradska_linija*> lines;//kada se brise lines, brisu se i gradske linije, treba promeniti to, posto imaju oznaku onda
-	//ova lista nema operator() za brisanje, tako da zasto ne bi samo ne brisao iz liste, nego rucno u mainu
-	//isto i za automobile listu
-	//za evidenciju neka ostane jer ima operator()?
-	//u sustini najbolje je i bez pokazivaca u prvoj
+	Lista<Gradska_linija*> lines;
+
 };
 
 #endif
