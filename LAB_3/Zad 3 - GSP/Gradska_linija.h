8ab4364bdd054209cdd202636f835f9333664d91
Nikola Cvetanovic <44877969+NikolaCv@users.noreply.github.com>
2019-12-25 14:43:29 +0100

    Update Gradska_linija.h

--- LAB_3/Zad 3 - GSP/Gradska_linija.h
+++ LAB_3/Zad 3 - GSP/Gradska_linija.h
@@ -9,20 +9,20 @@
 class Gradska_linija
 {
 public:
-	Gradska_linija(const string& text_tag, Lista<Stajaliste*> list);
+	Gradska_linija(const string& text_tag, Lista<Stajaliste*> list);//TODO
 	Gradska_linija(const Gradska_linija& line) = default;
 	Gradska_linija(Gradska_linija&& line) = default;				//moze default???
 	Gradska_linija& operator=(const Gradska_linija& line) = default;
 	Gradska_linija& operator=(Gradska_linija&& line) = default;
 	~Gradska_linija() = default;
 
-	friend int operator&(const Gradska_linija& line1, const Gradska_linija& line2);
+	friend int operator&(const Gradska_linija& line1, const Gradska_linija& line2);//TODO
 
 	friend ostream& operator<<(ostream& out, const Gradska_linija& line);
 
 private:
 	string text_tag;
-	Lista<Stajaliste*> list;//kako se zadaje?
+	Lista<Stajaliste*> stations;//kako se zadaje?//takodje dodati linije u stanicama
 };
 
 #endif
